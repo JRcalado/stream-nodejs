4693303f473c173dde480d7773a90f53e09158bd
José Ricardo Calado <josericardocalado@MacBook-Air-de-Jose.local>
2023-03-14 21:19:39 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import http from 'http'
+import { Readable } from 'stream'
+import { randomUUID } from 'crypto'
+
+
+function* run (){
+    for (let index = 0; index <= 99; index++ ){
+        const data = {
+            id: randomUUID(),
+            name: `Erick-${index}` 
+        }
+
+        yield data
+    }
+}
+
+
+async function handler(request, response){
+  const readable =  new Readable({
+       read() {
+        for(const data of run()){
+          console.log(`sending`,data)   
+          this.push(JSON.stringify(data) + "\n")
+        }
+
+         // para informa qie os dados acacabaram
+         this.push(null);
+       }
+       
+      
+    })
+
+    readable
+        .pipe(response)
+
+
+}
+
+
+http.createServer(handler)
+.listen(3000)
+.on('listening', () => console.log('server running at 3000') );
